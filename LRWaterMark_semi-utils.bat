9f3c1bb7f9a55c47324be18bf9e0164f824e9f7f
Xu,Dingmin <Alex99@users.noreply.github.com>
2024-06-27 21:46:48 +0800

    Update Config

--- LRWaterMark_semi-utils.bat
+++ LRWaterMark_semi-utils.bat
@@ -2,8 +2,8 @@
 echo 请使用ANSI编码
 %echo 若提示找不到文件，请编辑LRWaterMark_semi-utils.bat以修改其指向的目录
 cd %~dp0
-python "main.py" -c config.yaml --input_dir "D:\00-OnedriveRoot\002-mail.sdu.edu.cn\OneDrive - mail.sdu.edu.cn\LightroomOutput\JPEG" --output_dir "D:/00-OnedriveRoot/002-mail.sdu.edu.cn/OneDrive - mail.sdu.edu.cn/LightroomOutput/JPEGwithWaterMark"
-python "main.py" -c config.yaml --input_dir "D:\00-OnedriveRoot\002-mail.sdu.edu.cn\OneDrive - mail.sdu.edu.cn\LightroomOutput\JPEGAAA" --output_dir "D:/00-OnedriveRoot/002-mail.sdu.edu.cn/OneDrive - mail.sdu.edu.cn/LightroomOutput/JPEGAAAwithWaterMark"
-python "main.py" -c config.yaml --input_dir "D:\00-OnedriveRoot\002-mail.sdu.edu.cn\OneDrive - mail.sdu.edu.cn\LightroomOutput\JPEG" --output_dir "D:/00-OnedriveRoot/002-mail.sdu.edu.cn/OneDrive - mail.sdu.edu.cn/LightroomOutput/JPEGAAAwithWaterMark-wechat" --resolution 2155 --quality 90 --shape 2.1 --file_younger_than 7 --force_y_padding True
+python "main.py" -c config.yaml --input_dir "D:\00-OnedriveRoot\001-mail.sdu.edu.cn\OneDrive - mail.sdu.edu.cn\LightroomOutput\JPEG" --output_dir "D:\00-OnedriveRoot\001-mail.sdu.edu.cn\OneDrive - mail.sdu.edu.cn\LightroomOutput\JPEGwithWaterMark"
+python "main.py" -c config.yaml --input_dir "D:\00-OnedriveRoot\001-mail.sdu.edu.cn\OneDrive - mail.sdu.edu.cn\LightroomOutput\JPEGAAA" --output_dir "D:\00-OnedriveRoot\001-mail.sdu.edu.cn\OneDrive - mail.sdu.edu.cn\LightroomOutput\JPEGAAAwithWaterMark"
+python "main.py" -c config.yaml --input_dir "D:\00-OnedriveRoot\001-mail.sdu.edu.cn\OneDrive - mail.sdu.edu.cn\LightroomOutput\JPEG" --output_dir "D:\00-OnedriveRoot\001-mail.sdu.edu.cn\OneDrive - mail.sdu.edu.cn\LightroomOutput\JPEGAAAwithWaterMark-wechat" --resolution 2155 --quality 90 --shape 2.1 --file_younger_than 7 --force_y_padding True
 python "main.py" -c config.yaml --input_dir "D:\Adobe_Cache\HXX" --output_dir "D:\01_Backups\RMN10Pro\徐定民\00Files\xiaxia"
 pause
